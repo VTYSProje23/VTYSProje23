f11c6e09320d083d9b3c94a85e40734e7886334a
SÜLEYMAN ABBUD <73120626+suleymanabbud@users.noreply.github.com>
2022-12-15 21:50:54 +0300

    Update veriEkleme .js
    Hatalrı düzenlendi , Kıta tabloları eklendi.

--- veriEkleme .js
+++ veriEkleme .js
@@ -1,46 +1,55 @@
-const { Console } = require('console');
-const { format } = require('mysql');
-const { Client} = require('pg')
-const client =new Client({
-  host:"localhost",
-  user:"postgres",
-  password:'121081',
-  port:5432,
-  database:"vtys2"
-})
-fs = require("fs")
+var mysql = require('mysql');
+var fs = require("fs")
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "121081",
+  database: "vtys"
+});
 
 var isim = []
 var resim = []
 var tehlike = []
 var aciklama = []
+var kita= []
 
-fs.readFile('Bilgiler.txt',function(err,data){
+fs.readFile('bilgi.txt',function(err,data){
     if(err) throw err;
-    bilgi= data.toString().split("#");
+    bilgi= data.toString().split("\n");
     
-    for(i=0; i<90; i++){
-        isim[i] = bilgi[i]
-        resim[i] = bilgi[i+90]
-        tehlike[i] = bilgi[i+180]
-        aciklama[i] = bilgi[i+270]
+    for(i=0; i<760; i+=5){
+        isim[i/5] = bilgi[i]
+        resim[i/5] = bilgi[i+1]
+        tehlike[i/5] = bilgi[i+2]
+        aciklama[i/5] = bilgi[i+3]
+        kita[i/5]=bilgi[i+4]
         
-
     }
-    client.connect(function(err){
-    for(j=0; j<=90; j++){
-      
-        if(err) throw err;
+})    
 
-        var sorgu= "INSERT INTO tb_canlilar (tur_id,isim,resim,tehlike,aciklama) values ('"+(j+1)+"','"+isim[j]+"','"+resim[j]+"','"+tehlike[j]+"','"+aciklama[j]+"')" 
-         client.query(sorgu,function(err){
-        
-             if(err) throw err;
-           
-              console.log("Eklendi");
-         
-           });
-         
-    }
-  });
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var kita_ = ""
+    for(j=0; j<152; j++){
+        if(kita[j]==1){
+            kita_ = 'tb_avrupa'
+          }else if(kita[j]==2){
+            kita_ ='tb_asya'
+          }else if(kita[j]==3){
+            kita_ ='tb_afrika'
+          }else if(kita[j]==4){
+            kita_ ='tb_gamerika'
+          }else if(kita[j]==5){
+            kita_ ='tb_kamerika'
+          }else{
+            kita_ = 'tb_avustralya'
+          }
+        var sql = 'INSERT INTO '+kita_+' (tur_id,isim,resim,tehlike,aciklama) values ("'+(j+1)+'","'+isim[j]+'","'+resim[j]+'","'+tehlike[j]+'","'+aciklama[j]+'")';
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Eklendi");
     });
+    }
+});
